9120b5d47ee453a608a207fcf8c55592c39fb888
Steve Kaliski <sjkaliski@gmail.com>
2012-09-07 11:10:55 -0400

    cleaning it up

--- numeric.ly.js
+++ numeric.ly.js
@@ -1,73 +1,85 @@
 /*
 * numeric.ly
 * author: steve kaliski
-* feb 2012
+* last modified: sep 2012
 *-----------------------------------------------------------------------------*/
 
 var numeric = {
 
   //basic addition of vals in an array
-  addition: function(arr){
-    var total = 0;
-    for(var i = 0 ; i < arr.length ; i++){
-      total = total + arr[i];
+  addition: function(arr) {
+    if (Object.prototype.toString.call(arr) === '[object Array]') {
+      var total = 0;
+      for (var i = 0 ; i < arr.length ; i++) {
+        total = total + arr[i];
+      }
+      return total;
+    } else {
+      throw new Error('ERROR: Input must be of type Array');
     }
-    return total;
   },
 
   //subtract items in an array (descending)
-  subtraction: function(arr){
-    var total = arr[arr.length - 1];
-    for (var i = arr.length - 2; i >= 0; i--) {
-      total -= arr[i]; 
+  subtraction: function(arr) {
+    if (Object.prototype.toString.call(arr) === '[object Array]') {
+      var total = arr[arr.length - 1];
+      for (var i = arr.length - 2; i >= 0; i--) {
+        total -= arr[i]; 
+      }
+      return total;
+    } else { 
+      throw new Error('ERROR: Input must be of type Array');
     }
-    return total;
   },
 
   //product of items in array
-  product: function(arr){
-    var total = arr[0];
-    for (var i = 1; i < arr.length; i++) {
-      total = total * arr[i];
+  product: function(arr) {
+    if (Object.prototype.toString.call(arr) === '[object Array]') {
+      var total = arr[0];
+      for (var i = 1; i < arr.length; i++) {
+        total = total * arr[i];
+      }
+      return total;
+    } else {
+      throw new Error('ERROR: Input must be of type Array');
     }
-    return total;
   },
 
   //greatest common denominator
-  gcd: function(num1, num2){
+  gcd: function(num1, num2) {
     var result;
-    if(num1 > num2){
-      for(var i = 0 ; i <= num2 ; i++){
-        if(num2%i === 0){
-          if(num1%i === 0){
+    if (num1 > num2) {
+      for (var i = 0 ; i <= num2 ; i++) {
+        if (num2 % i === 0) {
+          if (num1 % i === 0) {
             result = i;
           }
         }
       }
       return result;
-    }else if(num2 > num1){
-      for(var i = 0 ; i <= num2 ; i++){
-        if(num1%i === 0){
-          if(num2%i === 0){
+    } else if (num2 > num1) {
+      for (var i = 0 ; i <= num2 ; i++) {
+        if (num1 % i === 0) {
+          if (num2 % i === 0) {
             result = i;
           }
         }
       }
       return result;
-    }else{
-      result = num1*num2/num1;
+    } else {
+      result = num1 * num2 / num1;
       return result;
     }
   },
 
   //least common multiple
-  lcm: function(num1, num2){
-    var result = Math.abs(num1*num2) / gcd(num1,num2);
+  lcm: function(num1, num2) {
+    var result = Math.abs(num1 * num2) / gcd(num1,num2);
     return result;
   },
 
   //evaluate function at val (func passed as a string)
-  evaluate: function(func, val){
+  evaluate: function(func, val) {
     return parseFloat(eval("with(Math){var x = " + val + ";" + func+'};'));
   },
 
@@ -77,69 +89,70 @@ var numeric = {
   calculus: {
 
     //function, point at which differentiation occurs (func passed as string)
-    pointDiff: function(func, point){
+    pointDiff: function(func, point) {
       var a = evaluate(func, point - .00001);
       var b = evaluate(func, point + .00001);
-      return (b-a)/(.00002);
+
+      return (b - a) / (.00002);
     },
 
     //calculate riemann integral (left hand) (func passed as string)
-    riemann: function(func, start, finish, n){
-      var inc = (finish - start)/n;
+    riemann: function(func, start, finish, n) {
+      var inc = (finish - start) / n;
       var result = 0;
-      for (var i = start; i < finish; i+= inc) {
+      for (var i = start; i < finish; i += inc) {
         result += (evaluate(func, i) * inc);
       }
       return result;
     },
 
     //estimate integral with adaptive simpson quadrature
-    simpsonDef: function(func, a, b){
+    simpsonDef: function(func, a, b) {
       var c = (a + b) / 2;
       var d = Math.abs(b - a) / 6;
       return d * (evaluate(func, a) + 4 * evaluate(func, c) + evaluate(func, b));
     },
-    simpsonRecursive: function(func, a, b, eps, whole){
+    simpsonRecursive: function(func, a, b, eps, whole) {
       var c = a + b;
       var left = simpsonDef(func, a, c);
       var right = simpsonDef(func, c, b);
-      if(Math.abs(left + right - whole) <= 15 * eps){
+      if (Math.abs(left + right - whole) <= 15 * eps) {
         return left + right + (left + right - whole) / 15;
-      }else{
-        return simpsonRecursive(func, a, c, eps/2, left) + simpsonRecursive(func, c, b, eps/2, right);
+      } else {
+        return simpsonRecursive(func, a, c, eps/2, left) + simpsonRecursive(func, c, b, eps / 2, right);
       }
     },
     //execute this
-    adaptiveSimpson: function(func, a, b, eps){
+    adaptiveSimpson: function(func, a, b, eps) {
       return simpsonRecursive(func, a, b, eps, simpsonDef(func, a, b));
     },
 
     //take the limit of a function (left, middle, or right)
-    limit: function(func, point, approach){
-      if(approach === 'left'){
+    limit: function(func, point, approach) {
+      if (approach === 'left') {
         return evaluate(func, point - .001);
-      }else if(approach === 'right'){
+      } else if (approach === 'right') {
         return evaluate(func, point + .001);
-      }else if(approach === 'middle'){
+      } else if (approach === 'middle') {
         return (limit(func, point, 'left') + limit(func, point, 'right')) / 2;
-      }else{
+      } else {
         throw new Error('Approach not provided');
       }
     },
 
     //return taylor polynomial for function (func) at point (point) to degree (degree: 0, 1 and 2 for now)
-    taylor: function(func, point, degree){
-      if(degree == 0){
+    taylor: function(func, point, degree) {
+      if (degree == 0) {
         
-      }else if(degree == 1){
+      } else if (degree == 1) {
         
-      }else if(degree == 2){
+      } else if (degree == 2) {
         
       }
     },
 
     //integrate taylor polynomial for some func
-    polyIntegrate: function(func){
+    polyIntegrate: function(func) {
       var taylor = taylor(func, 0, 8);
     }
   },
@@ -163,8 +176,8 @@ var numeric = {
   matrix: {
     
     //add two arrays (arrA and arrB)
-    addition: function(arrA, arrB){
-      if((arrA.length === arrB.length) && (arrA[0].length === arrB[0].length)){
+    addition: function(arrA, arrB) {
+      if ((arrA.length === arrB.length) && (arrA[0].length === arrB[0].length)) {
         var result = new Array(arrA.length);
         for (var i = 0; i < arrA.length; i++) {
           result[i] = new Array(arrA[i].length);
@@ -173,13 +186,13 @@ var numeric = {
           }
         }
         return result;
-      }else{
+      } else {
         throw new Error('Array mismatch');
       }
     },
 
     //multiple array (arr) by a scalar (val)
-    scalar: function(arr,val){
+    scalar: function(arr,val) {
       for (var i = 0; i < arr.length; i++) {
         for (var j = 0; j < arr[i].length; j++) {
           arr[i][j] = val * arr[i][j];
@@ -189,11 +202,11 @@ var numeric = {
     },
 
     //transpose array
-    transpose: function(arr){
+    transpose: function(arr) {
       var result = new Array(arr[0].length);
       for (var i = 0; i < arr[0].length; i++) {
         result[i] = new Array(arr.length);
-        for(var j = 0; j < arr.length; j++){
+        for (var j = 0; j < arr.length; j++){
           result[i][j] = arr[j][i];
         }
       }
@@ -201,11 +214,11 @@ var numeric = {
     },
 
     //return identity matrix of dimension n x n
-    identity: function(n){
+    identity: function(n) {
       var result = new Array(n);
-      for(var i = 0 ; i < n ; i++){
+      for (var i = 0 ; i < n ; i++) {
         result[i] = new Array(n);
-        for(var j = 0 ; j < n ; j++){
+        for (var j = 0 ; j < n ; j++) {
           result[i][j] = (i === j) ? 1 : 0;
         }
       }
@@ -213,43 +226,51 @@ var numeric = {
     },
 
     //dot product
-    dotproduct: function(vectorA, vectorB){
-      if(vectorA.length === vectorB.length){
+    dotproduct: function(vectorA, vectorB) {
+      if (vectorA.length === vectorB.length) {
         var result = 0;
-        for(var i = 0 ; i < vectorA.length ; i++){
+        for (var i = 0 ; i < vectorA.length ; i++) {
           result += vectorA[i]*vectorB[i];
         }
         return result;
-      }else{
+      } else {
         throw new Error("Vector mismatch");
       }
     },
 
     //multiply two matrices
-    multiply: function(arrA, arrB){
-      if(arrA[0].length === arrB.length){
+    multiply: function(arrA, arrB) {
+      if (arrA[0].length === arrB.length) {
         var result = new Array(arrA.length);
-        for(var i = 0 ; i < arrA.length ; i++){
+        
+        for (var i = 0 ; i < arrA.length ; i++) {
           result[i] = new Array(arrB[0].length);
         }
+
         var arrB_T = transpose(arrB);
-        for(var i = 0 ; i < result.length ; i++){
-          for(var j = 0 ; j < result[i].length ; j++){
+        
+        for (var i = 0 ; i < result.length ; i++) {
+          for (var j = 0 ; j < result[i].length ; j++) {
             result[i][j] = dotproduct(arrA[i],arrB_T[j]);
           }
         }
         return result;
-      }else{
+      } else {
         throw new Error("Array mismatch");
       }
     },
 
     //determinant, only for n x n -> 2 and 3
-    determinant: function(m){
-      if((m.length === 2) && (m[0].length === 2)){
-        return m[0][0]*m[1][1] - m[0][1]*m[1][0];
-      }else if((m.length === 3) && (m[0].length === 3)){
-        return m[0][0]*m[1][1]*m[2][2] + m[0][1]*m[1][2]*m[2][0] + m[0][2]*m[1][0]*m[2][1] - m[0][2]*m[1][1]*m[2][0] - m[0][1]*m[1][0]*m[2][2] - m[0][0]*m[1][2]*m[2][1];
+    determinant: function(m) {
+      if ((m.length === 2) && (m[0].length === 2)) {
+        return m[0][0] * m[1][1] - m[0][1] * m[1][0];
+      } else if ((m.length === 3) && (m[0].length === 3)) {
+        return m[0][0] * m[1][1] * m[2][2] 
+          + m[0][1] * m[1][2] * m[2][0] 
+          + m[0][2] * m[1][0] * m[2][1] 
+          - m[0][2] * m[1][1] * m[2][0] 
+          - m[0][1] * m[1][0] * m[2][2] 
+          - m[0][0] * m[1][2] * m[2][1];
       }
     }
   },
@@ -260,19 +281,19 @@ var numeric = {
   prime: {
 
     //standard prime evaluation (no consideration towards efficiency)
-    simple: function(val){
-      if(val == 1){
+    simple: function(val) {
+      if (val == 1) {
         return false;
-      }else if(val == 2){
+      } else if (val == 2) {
         return true;
-      }else if(val != null){
+      } else if (val != null) {
         var start = 2;
         var result = true;
-        while(start < val){
-          if(val % start === 0){
+        while (start < val) {
+          if (val % start === 0) {
             result = false;
             break;
-          }else{
+          } else {
             start++;
           }
         }
@@ -281,7 +302,7 @@ var numeric = {
     },
 
     //determine primality using elliptic curve testing
-    elliptic: function(){
+    elliptic: function() {
       return null;
     }
   },
@@ -292,40 +313,41 @@ var numeric = {
   statistic: {
     
     //get mean value of the numbers in an array
-    mean: function(arr){
+    mean: function(arr) {
       var count = arr.length;
       var sum = addition(arr);
       return sum/count;
     },
 
     //get median value of the numbers in an array
-    median: function(arr){
+    median: function(arr) {
       var count = arr.length;
       var middle;
-      if(count % 2 === 0){
+      if (count % 2 === 0) {
         return (arr[count/2] + arr[(count/2 - 1)])/2;
-      }else{
+      } else {
         return arr[Math.floor(count / 2)];
       }
     },
 
     //get mode value (most common value) of the numbers in an array
-    mode: function(arr){
+    mode: function(arr) {
       //sort array
       var maxIndex = 0, maxOccurence = 0, tempIndex = 0, tempOccurence = 0;
       arr = arr.sort();
-      while(tempIndex < arr.length) {
+      while (tempIndex < arr.length) {
         for (var j = tempIndex; j < arr.length; j++) {
-          if(arr[j] == arr[tempIndex]){
+          if (arr[j] == arr[tempIndex]) {
             tempOccurence++;
-          }else{
+          } else {
             break;
           }
         }
-        if(tempOccurence > maxOccurence){
+        if (tempOccurence > maxOccurence) {
           maxOccurence = tempOccurence;
           maxIndex = tempIndex;
         }
+
         tempIndex = j;
         tempOccurence = 0;
       }
@@ -333,19 +355,19 @@ var numeric = {
     },
 
     //generate a random sample of n-values within lower/upper bounds
-    randomSample: function(lower,upper,n){
+    randomSample: function(lower,upper,n) {
       var sample = new Array();
       var temp;
-      for(var i = 0 ; i < n ; i++){
+      for (var i = 0 ; i < n ; i++) {
         temp = Math.random()*upper;
-        if(temp > lower)
+        if (temp > lower)
           sample[i] = temp;
       }
       return sample;
     },
 
     //standard deviation
-    standardDev: function(arr){
+    standardDev: function(arr) {
       var count = arr.length;
       var mean = mean(arr);
       var squaredArr = new Array();
@@ -356,17 +378,17 @@ var numeric = {
     },
 
     //correlation of two arrays
-    correlation: function(arrX,arrY){
-      if(arrX.length == arrY.length){
+    correlation: function(arrX,arrY) {
+      if (arrX.length == arrY.length) {
         var numerator = 0;
-        var denominator = (arrX.length)*(standardDev(arrX))*(standardDev(arrY));
+        var denominator = (arrX.length) * (standardDev(arrX)) * (standardDev(arrY));
         var xMean = mean(arrX);
         var yMean = mean(arrY);
-        for(var i = 0 ; i < arrX.length ; i++){
+        for (var i = 0 ; i < arrX.length ; i++) {
           numerator += (arrX[i] - xMean) * (arrY[i] - yMean);
         }
         return numerator / denominator;
-      }else{
+      } else {
         throw new Error('Array mismatch');
       }
     }
