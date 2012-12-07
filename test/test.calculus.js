932af0e2a660e6e8428f328ea6f83fa4677ffcb1
Steve Kaliski <sjkaliski@gmail.com>
2012-12-07 01:30:59 -0500

    starting to use error bound

---
+++ test/test.calculus.js
@@ -0,0 +1,17 @@
+var assert = require('assert');
+var numeric = require('../index.js');
+var calculus = numeric.calculus;
+
+suite('numeric', function() {
+
+  console.log('\n\n\033[34mTesting Calculus Mathematics\033[0m');
+
+  test('pointDiff should return the derivative at a point, provided function', function(done) {
+    var func = function(x) {
+      return 2 * x + 2;
+    };
+
+    assert.equal(true, 2 - calculus.pointDiff(func, 5) < numeric.ERROR_BOUND);
+    done();
+  });
+});
