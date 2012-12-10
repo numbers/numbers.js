b26b8e578cb56a82911f7de657b4717490ac7b45
David Byrd <david@byrdhou.se>
2012-12-10 03:53:32 -0500

    fixed original collatz test

--- test/useless.test.js
+++ test/useless.test.js
@@ -10,7 +10,7 @@ suite('numeric', function() {
   test('collatz should populate the given array with a collatz sequence', function (done) {
     var result = [];
     useless.collatz(7, result);
-    assert.equal([7,22,11,34,17,52,26,13,40,20,10,5,16,8,4,2,1].join(), result.join());
+    assert.equal([7,22,11,34,17,52,26,13,40,20,10,5,16,8,4,2,1].join(','), result.join(','));
     done();
   });
 });
