30372423f03a152c408fe52a3d1d33d1a38d5184
Jason Hutchinson <zikes@zikes.me>
2012-12-12 17:21:20 -0600

    add statistic.rSquared and update statistic.exponentialRegression to include it

--- test/statistic.test.js
+++ test/statistic.test.js
@@ -80,10 +80,23 @@ suite('numbers', function() {
 
     var regression_function = statistic.exponentialRegression(input);
 
+    assert.equal(0.8491729985314136, regression_function.rSquared);
+
     assert.deepEqual(output, regression_function(basic.range(1, input.length)));
     assert.equal(9.077131929916444, regression_function(1));
     assert.equal(4.769782016165231, regression_function(15));
     done();
   });
 
+  test('should return an appropriate Coefficient of Determination for a given dataset and regression', function (done) {
+    var input = [10,9,8,8,7,7,6,6.5,6.4,6.3,6.2];
+    var output = [
+      9.077131929916444, 8.66937771538526, 8.279940244595563, 7.907996710352883,
+      7.552761266818376, 7.213483369166244, 6.8894461878255076, 6.579965093955639,
+      6.284386212956255, 6.002085042954625, 5.732465135352174
+    ];
+
+    assert.equal(0.8491729985314136, statistic.rSquared(input, output));
+    done();
+  });
 });
