791aaa219df21b4f9b87202f258bc129b30c63b4
Ethan Resnick <ethan.resnick@gmail.com>
2012-12-20 00:19:58 -0500

    Vector Type Stub

---
+++ test/vector-type.test.js
@@ -0,0 +1,11 @@
+var assert = require('assert');
+var numbers = require('../index.js');
+var matrix = numbers.matrix;
+var Matrix = numbers.matrix.Matrix;
+
+suite('numbers', function() {
+
+  console.log('\n\n\033[34mTesting Vector Type Mathematics\033[0m');
+
+
+});
