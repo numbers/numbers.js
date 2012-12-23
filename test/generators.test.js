c804c72f54d76007f7baf69d041c08435ffbf57a
Kartik Talwar <hi@kartikt.com>
2012-12-23 18:38:11 -0500

    Added fibonacci generator

---
+++ test/generators.test.js
@@ -0,0 +1,16 @@
+var assert = require('assert');
+var numbers = require('../index.js');
+var generate = numbers.generate;
+
+suite('numbers', function() {
+
+  console.log('\n\n\033[34mTesting Generators\033[0m');
+
+  //numbers.generate.fibonacci
+  test('Should generate the nth fibonacci number', function (done) {
+    assert.equal(55, generate.fibonacci(10));
+    assert.equal(12586269025, generate.fibonacci(50));
+    assert.equal(14472334024676221, generate.fibonacci(79));
+    done();
+  });
+});
