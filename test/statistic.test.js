523152c4d80868c80fd3fe778017eb5259b5306b
Ethan Resnick <ethan.resnick@gmail.com>
2013-01-25 08:53:48 -0500

    Create the Sample class (with tests) and move some stats stuff to it

--- test/statistic.test.js
+++ test/statistic.test.js
@@ -7,12 +7,6 @@ suite('numbers', function() {
 
   console.log('\n\n\033[34mTesting Statistics Mathematics\033[0m');
 
-  test('mean should return average value amongst integers in an array', function(done) {
-    var res = statistic.mean([0, 1, 2]);
-    assert.equal(1, res);
-    done();
-  });
-
   test('median should return middle value in array for a sorted array with an odd number of values', function(done) {
     var res1 = statistic.median([0, 2, 15]);
     assert.equal(2, res1);
@@ -77,72 +71,6 @@ suite('numbers', function() {
     });
 
     assert.equal(5, res.length);
-    done();
-  });
-
-  test('should return the standard deviation of an array of numbers', function(done) {
-    var res = statistic.standardDev([-5, -4, -1, 0, 5, 100]);
-    assert.equal(true, res - numbers.EPSILON < 37.777 < res + numbers.EPSILON);
-    done();
-  });
-
-  test('should return correlation between two arrays', function(done) {
-    var arr1 = [-5, -4, -1, 0, 5, 100];
-    var arr2 = [-6, 5, 2, 5, 2, 6];
-
-    var res = statistic.correlation(arr1, arr2);
-
-    assert.equal(0.43413125731182334, res);
-    done();
-  });
-
-  test('should return a function to calculate the linear regression of a set of points', function (done) {
-    var arrX = [1,2,3,4,5,7,8,9];
-    var arrY = [1,2,3,4,5,7,7,9];
-
-    var regression_function = statistic.linearRegression(arrX,arrY);
-
-    assert.equal(19.07218683651805, regression_function(20));
-    done();
-  });
-
-  test('should return a function to calculate the exponential regression of an array of numbers', function (done) {
-    var input = [10,9,8,8,7,7,6,6.5,6.4,6.3,6.2];
-    var output = [
-      9.077131929916444, 8.66937771538526, 8.279940244595563, 7.907996710352883,
-      7.552761266818376, 7.213483369166244, 6.8894461878255076, 6.579965093955639,
-      6.284386212956255, 6.002085042954625, 5.732465135352174
-    ];
-
-    var regression_function = statistic.exponentialRegression(input);
-
-    assert.equal(0.8491729985314136, regression_function.rSquared);
-
-    assert.deepEqual(output, regression_function(basic.range(1, input.length)));
-    assert.equal(9.077131929916444, regression_function(1));
-    assert.equal(4.769782016165231, regression_function(15));
-    done();
-  });
-
-  test('should return an appropriate Coefficient of Determination for a given dataset and regression', function (done) {
-    var input = [10,9,8,8,7,7,6,6.5,6.4,6.3,6.2];
-    var output = [
-      9.077131929916444, 8.66937771538526, 8.279940244595563, 7.907996710352883,
-      7.552761266818376, 7.213483369166244, 6.8894461878255076, 6.579965093955639,
-      6.284386212956255, 6.002085042954625, 5.732465135352174
-    ];
-
-    assert.equal(0.8491729985314136, statistic.rSquared(input, output));
-    done();
-  });
-
-  test('should return covariance between two arrays', function(done) {
-    var arr1 = [-5, -4, -1, 0, 5, 100];
-    var arr2 = [-6, 5, 2, 5, 2, 6];
-
-    var res = statistic.covariance(arr1, arr2);
-
-    assert.equal(66.05555555555556, res);
     done();
   });
 });
--- test/type-sample.js
+++ test/type-sample.js
@@ -42,4 +42,64 @@ suite('numbers', function() {
     assert.equal(true, numbers.nearlyEquals(37.777, res));
     done();
   });
+  
+  test('should return covariance between two samples', function(done) {
+    var arr1 = [-5, -4, -1, 0, 5, 100];
+    var arr2 = [-6, 5, 2, 5, 2, 6];
+
+    var res = numbers.createSample(arr1).covariance(arr2);
+
+    assert.equal(numbers.nearlyEquals(66.05555555555556, res), true);
+    done();
+  });
+
+  test('should return correlation between two samples', function(done) {
+    var arr1 = [-5, -4, -1, 0, 5, 100];
+    var arr2 = [-6, 5, 2, 5, 2, 6];
+
+    var res = numbers.createSample(arr1).correlation(arr2);
+
+    assert.equal(true, numbers.nearlyEquals(0.43413125731182334, res));
+    done();
+  });
+  
+  test('should return a function to calculate the linear regression of a set of points', function (done) {
+    var arrX = [1,2,3,4,5,7,8,9];
+    var arrY = [1,2,3,4,5,7,7,9];
+
+    var regression_function = numbers.createSample(arrX).linearRegression(arrY);
+
+    assert.equal(true, numbers.nearlyEquals(19.0721868365, regression_function(20)));
+    done();
+  });
+  
+  test.skip('should return a function to calculate the exponential regression of an array of numbers', function (done) {
+    var input = [10,9,8,8,7,7,6,6.5,6.4,6.3,6.2];
+    var output = [
+      9.077131929916444, 8.66937771538526, 8.279940244595563, 7.907996710352883,
+      7.552761266818376, 7.213483369166244, 6.8894461878255076, 6.579965093955639,
+      6.284386212956255, 6.002085042954625, 5.732465135352174
+    ];
+
+    var regression_function = statistic.exponentialRegression(input);
+
+    assert.equal(0.8491729985314136, regression_function.rSquared);
+
+    assert.deepEqual(output, regression_function(basic.range(1, input.length)));
+    assert.equal(9.077131929916444, regression_function(1));
+    assert.equal(4.769782016165231, regression_function(15));
+    done();
+  });
+
+  test.skip('should return an appropriate Coefficient of Determination for a given dataset and regression', function (done) {
+    var input = [10,9,8,8,7,7,6,6.5,6.4,6.3,6.2];
+    var output = [
+      9.077131929916444, 8.66937771538526, 8.279940244595563, 7.907996710352883,
+      7.552761266818376, 7.213483369166244, 6.8894461878255076, 6.579965093955639,
+      6.284386212956255, 6.002085042954625, 5.732465135352174
+    ];
+
+    assert.equal(0.8491729985314136, statistic.rSquared(input, output));
+    done();
+  });
 });
